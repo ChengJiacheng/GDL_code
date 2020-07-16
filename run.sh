bfa3bb05676731006ce1ef12f10552c39ee74f68
ChengJiacheng <chinging@foxmail.com>
2020-07-16 04:01:04 +0000

    none

--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
 # python -u train_distillation.py > log10.out
-python -u train_supervised.py > logx4.out
+# python -u train_supervised.py > logx4.out
+runipy 03_03_vae_digits_train.ipynb OutputNotebook_0303.ipynb
